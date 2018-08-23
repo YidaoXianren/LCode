4cdb5ec0c755566405880acee52eaf9f3588be43
YidaoXianren <42054519+YidaoXianren@users.noreply.github.com>
2018-08-23 11:57:34 -0400

    Update peak_index_in_a_mountaion_array.cpp

--- peak_index_in_a_mountaion_array.cpp
+++ peak_index_in_a_mountaion_array.cpp
@@ -1,3 +1,4 @@
+//method 1:
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& A) {
@@ -13,3 +14,29 @@ public:
         return idx;
     }
 };
+
+
+//method 2:
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int i = 0;
+        while (A[i] < A[i+1]) i++;
+        return i;
+    }
+}
+
+//method 3:
+//binary search
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int lo = 0, hi = A.length - 1;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (A[mi] < A[mi + 1])
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+        return lo;
+    }
+}
